f3ea1aef651dccd5cc85ef303bcb2b4cbd4ece01
Emily Webber <egwebber@amazon.com>
2025-03-21 12:36:50 -0400

    Update install_run.sh

--- scripts/install_run.sh
+++ scripts/install_run.sh
@@ -22,3 +22,22 @@ sudo apt-get install aws-neuronx-collectives=2.* -y
 sudo apt-get install aws-neuronx-tools -y
 export PATH=/opt/aws/neuron/bin:$PATH
 export LD_LIBRARY_PATH=/opt/amazon/efa/lib64:$LD_LIBRARY_PATH
+
+# set up the Python virtual environment
+# For python3.10: skip env setup if environment already exists
+sudo apt-get install python3.10-venv
+python3.10 -m venv aws_neuron_venv_pytorch_p310
+
+# Activate Python venv 
+source aws_neuron_venv_pytorch_p310/bin/activate 
+python -m pip install -U pip 
+
+# Install Jupyter notebook kernel
+pip install ipykernel 
+python3.10 -m ipykernel install --user --name aws_neuron_venv_pytorch_p310 --display-name "Python (torch-neuronx)"
+pip install jupyter notebook
+pip install environment_kernels
+
+# Install wget, awscli 
+python -m pip install wget 
+python -m pip install awscli
